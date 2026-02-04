db8693f88c4afd1862e4a8deabde1511b712c54e
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-02-04 15:34:59 +0545

    debugging the models

--- portfolio/migrations/0002_remove_project_image_project_created_at.py
+++ portfolio/migrations/0002_remove_project_image_project_created_at.py
@@ -17,7 +17,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='project',
             name='created_at',
-            field=models.DateTimeField(auto_now_add=True, default=1),
-            preserve_default=False,
+            field=models.DateTimeField(auto_now_add=True),
         ),
     ]
