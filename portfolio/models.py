251132985b8f546cd91c588b99ef49477a3dd896
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-02-04 15:30:43 +0545

    creating project model

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
-# Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     github_link = models.URLField()
     live_link = models.URLField(blank=True)
-    image = models.ImageField(upload_to='projects/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
