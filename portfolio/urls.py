168cf98a8391739119dbc4f27a096546e93a138c
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-02-04 15:24:50 +0545

    arranging the urls

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import home
+from .views import home, about, projects, contact
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about/', about, name='about'),
+    path('projects/', projects, name='projects'),
+    path('contact/', contact, name='contact'),
 ]
