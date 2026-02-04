724a90f68a04cf3e642a7a8ed9e4db56c16d8a6b
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-02-04 18:28:58 +0545

    update the contact in the admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from portfolio.models import Project
+from portfolio.models import Contact, Project
 
 # Register your models here.
 admin.site.register(Project)
+admin.site.register(Contact)
