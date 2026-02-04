f6ef55e112bdacf2b3e12c37b0d2b7e11a89ddbc
Sameer Dhakal <sameerdhakal1234@gmail.com>
2026-02-04 19:09:30 +0545

    creating forms.py for the email

--- portfolio/forms.py
+++ portfolio/forms.py
@@ -1,7 +1,6 @@
 from django import forms
-from .models import Contact
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ['name', 'email', 'message']
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
